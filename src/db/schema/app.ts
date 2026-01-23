246beb1b301c749e53c19f37453fce590906716a
Jie-Si Chen <jessie@paxai.com>
2026-01-23 12:08:37 +0800

    Refactor database schemas

--- src/db/schema/app.ts
+++ src/db/schema/app.ts
@@ -1,13 +1,38 @@
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { relations} from "drizzle-orm"
+import {integer, pgTable, timestamp, varchar} from "drizzle-orm/pg-core"
 
-// Define the 'demo_users' table
-export const demoUsers = pgTable('demo_users', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  email: text('email').notNull().unique(),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-});
+const timestamps = {
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().$onUpdate(() => new Date()).notNull()
+}
 
-// Export types for type-safe queries
-export type User = typeof demoUsers.$inferSelect;
-export type NewUser = typeof demoUsers.$inferInsert;
+export const departments = pgTable('departments', {
+    id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+    code: varchar('code', {length: 50}).notNull().unique(),
+    name: varchar('name', {length: 255}).notNull(),
+    description: varchar('description', {length: 255}),
+    ...timestamps
+})
+
+export const subjects = pgTable('subjects', {
+    id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+    departmentId: integer("department_id").references(() => departments.id, { onDelete: "restrict" }),
+    name: varchar('name', {length: 255}).notNull(),
+    code: varchar('code', {length: 50}).notNull().unique(),
+    description: varchar('description', {length: 255}),
+    ...timestamps
+})
+
+export const departmentRelations = relations(departments, ({many}) => ({subjects: many(subjects)}))
+
+export const subjectsRelations = relations(subjects, ({one, many}) => ({
+    department: one(departments, {
+        fields: [subjects.departmentId], 
+        references:[departments.id],
+    }) 
+}))
+
+export type Department = typeof departments.$inferSelect;
+export type NewDepartment = typeof departments.$inferInsert;
+export type Subject = typeof subjects.$inferSelect;
+export type NewSubject = typeof subjects.$inferInsert; 
