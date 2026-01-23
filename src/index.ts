8f003145e18913feadce043e0ee18078fe9e968a
Jie-Si Chen <jessie@paxai.com>
2026-01-23 14:08:47 +0800

    Implement route for subjects

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-
+import subjectsRouter from './routes/subjects.js'
 const app = express();
 const PORT = 8000;
 
 // JSON middleware
 app.use(express.json());
 
+app.use('/api/subjects', subjectsRouter)
 // Root GET route
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to University Management Dashboard API' });
